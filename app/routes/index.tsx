eddfacb437cbe7f8a6fa1bf0b2668dca00bfaf12
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-10-23 21:42:48 -0500

    fancy module view select

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Select, SelectItem, SelectPopover, useSelectState } from "ariakit"
 import cuid from "cuid"
 import { Maximize2, PlusSquare, SeparatorVertical, X, Zap } from "lucide-react"
 import { useContext, useState } from "react"
@@ -10,8 +11,8 @@ import {
 } from "react-mosaic-component"
 import { clearButtonClass } from "../ui/styles"
 
-export default function Home() {
-  const [mosaic, setMosaic] = useState<MosaicNode<string> | null>(null)
+export default function IndexPage() {
+  const [mosaic, setMosaic] = useState<MosaicNode<string> | null>(() => cuid())
 
   const addWindow = () => {
     setMosaic((prev) => {
@@ -44,7 +45,7 @@ export default function Home() {
           onChange={setMosaic}
           className=""
           renderTile={(id, path) => (
-            <CustomMosaicWindow windowId={id} windowPath={path} />
+            <ModuleWindow windowId={id} windowPath={path} />
           )}
           zeroStateView={
             <p className="absolute right-0 bottom-0 p-8 text-4xl opacity-25 select-none">
@@ -63,14 +64,13 @@ export default function Home() {
   )
 }
 
-function CustomMosaicWindow({
+function ModuleWindow({
   windowId: id,
   windowPath: path,
 }: {
   windowId: string
   windowPath: MosaicBranch[]
 }) {
-  const titleId = `window-title-${id}`
   return (
     <MosaicWindow
       title={id}
@@ -78,29 +78,56 @@ function CustomMosaicWindow({
       createNode={() => cuid()}
       renderToolbar={() => (
         <div className="flex items-center justify-between w-full px-3">
-          <h2 id={titleId}>{id}</h2>
-          <MosaicWindowControls windowPath={path} titleId={titleId} />
+          <ModuleViewSelect />
+          <ModuleWindowControls windowPath={path} />
         </div>
       )}
     >
-      <section className="w-full h-full bg-slate-800" aria-labelledby={titleId}>
+      <section className="w-full h-full bg-slate-800">
         <p>test</p>
       </section>
     </MosaicWindow>
   )
 }
 
-function MosaicWindowControls({
-  windowPath,
-  titleId,
-}: {
-  windowPath: MosaicBranch[]
-  titleId: string
-}) {
+function ModuleViewSelect() {
+  const options = ["Characters", "Clocks", "Dice", "Whiteboard"] as const
+
+  const select = useSelectState({
+    defaultValue: options[0],
+    gutter: 12,
+    animated: true,
+  })
+
+  return (
+    <div className="relative flex">
+      <Select
+        state={select}
+        className="flex items-center gap-1 flex-row-reverse text-lg font-medium hover:text-blue-300 p-2 -m-2 leading-none"
+      />
+      <SelectPopover
+        state={select}
+        className="bg-slate-700 flex flex-col rounded-md shadow-md overflow-clip min-w-[10rem] transition origin-top-left data-[enter]:scale-100 data-[enter]:opacity-100 data-[leave]:opacity-0 data-[leave]:scale-95"
+      >
+        {options.map((option) => (
+          <SelectItem
+            key={option}
+            value={option}
+            className="leading-none p-3 data-[active-item]:bg-black/25 cursor-pointer"
+          >
+            {option}
+          </SelectItem>
+        ))}
+      </SelectPopover>
+    </div>
+  )
+}
+
+function ModuleWindowControls({ windowPath }: { windowPath: MosaicBranch[] }) {
   const { mosaicActions } = useContext(MosaicContext)
   const { mosaicWindowActions } = useContext(MosaicWindowContext)
   return (
-    <nav className="flex items-center gap-3" aria-labelledby={titleId}>
+    <nav className="flex items-center gap-3">
       <button
         title="Split"
         className={clearButtonClass}
--- app/ui/styles.ts
+++ app/ui/styles.ts
@@ -4,5 +4,9 @@ export const maxWidthContainerClass = clsx(
   "mx-auto w-full max-w-screen-md px-4",
 )
 
-export const clearButtonClass =
-  "uppercase hover:text-blue-300 inline-flex items-center gap-1 text-lg active:transition-none active:scale-95 transition-transform p-4 -m-4"
+export const clearButtonClass = clsx(
+  "hover:text-blue-300 ring-inset",
+  "focus:outline-none focus:ring-2 focus:ring-inset focus-within:ring-2 focus-within:ring-inset [&_*:focus]:outline-none ring-blue-300",
+  "uppercase inline-flex items-center gap-1 text-lg p-2 -m-2",
+  "transition-transform active:transition-none active:scale-95",
+)
