668ded2811d5a0996c8283b6f90fdac2f67da56a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-10-24 21:26:14 -0500

    concurrent client hydration

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,17 @@
 import { RemixBrowser } from "@remix-run/react"
+import { startTransition } from "react"
 import { hydrateRoot } from "react-dom/client"
 
-hydrateRoot(document, <RemixBrowser />)
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(document, <RemixBrowser />)
+  })
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate)
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1)
+}
