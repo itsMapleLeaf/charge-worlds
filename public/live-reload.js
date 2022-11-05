d63a5b6df996636b0aae09d1b3e70ca86edd62f8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-11-05 09:47:22 -0500

    more readable live reload script

--- public/live-reload.js
+++ public/live-reload.js
@@ -1,26 +1,44 @@
 // the default remix live reload reloads before the server is actually up,
 // and this custom one pings the server to make sure it's up before reloading
 
-function tryReload() {
-  /** @type {AbortController | undefined} */
-  let controller
-  let interval = setInterval(() => {
-    controller?.abort()
-    controller = new AbortController()
-    fetch("/up", { signal: controller.signal })
-      .then((response) => {
-        if (response.ok) {
-          clearInterval(interval)
-          window.location.reload()
-        }
-      })
-      .catch(() => {})
-  }, 500)
+/**
+ * @param {RequestInfo | URL} url
+ * @param {RequestInit=} options
+ * @param {number=} timeout
+ * @returns {Promise<Response>}
+ */
+async function fetchWithTimeout(url, options, timeout = 1000) {
+  const controller = new AbortController()
+  const id = setTimeout(() => controller.abort(), timeout)
+  try {
+    return await fetch(url, { ...options, signal: controller.signal })
+  } finally {
+    clearTimeout(id)
+  }
+}
+
+/**
+ * @param {number | undefined} ms
+ * @returns {Promise<void>}
+ */
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function tryReload() {
+  while (true) {
+    await delay(500)
+    try {
+      const response = await fetchWithTimeout("/up", {}, 500)
+      if (response.ok) break
+    } catch (error) {}
+  }
+  window.location.reload()
 }
 
 let protocol = location.protocol === "https:" ? "wss:" : "ws:"
 let host = location.hostname
-let socketPath = protocol + "//" + host + ":" + 8002 + "/socket"
+let socketPath = `${protocol}//${host}:8002/socket`
 
 let ws = new WebSocket(socketPath)
 ws.addEventListener("message", (message) => {
