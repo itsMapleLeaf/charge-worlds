ce618bf4c7c70ffab7cb1d62cedee4e79c8b871e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-11-09 10:47:52 -0600

    get the most recently created world

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,8 @@ import { db } from "../core/db.server"
 
 export async function loader() {
   const world =
-    (await db.world.findFirst()) ??
-    (await db.world.create({ data: { name: "New World" } }))
+    (await db.world.findFirst({
+      orderBy: { createdAt: "desc" },
+    })) ?? (await db.world.create({ data: { name: "New World" } }))
   return redirect(route("/worlds/:worldId/dashboard", { worldId: world.id }))
 }
