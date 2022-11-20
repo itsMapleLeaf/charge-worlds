ac4ba2722340e6ed5ce2f1109f2dea72e80975f4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-11-20 13:09:34 -0600

    refactor character sheet structure
    lift some stuff up to reduce prop drilling, make components smaller and more self-contained

---
+++ app/routes/worlds.$worldId.characters/character-action-levels-editor.tsx
@@ -0,0 +1,92 @@
+import { ChevronsRight, Dices } from "lucide-react"
+import type { Character } from "../../generated/prisma"
+import { entriesTyped } from "../../helpers/entries-typed"
+import { DotCounter } from "../../ui/counter"
+import { labelTextClass } from "../../ui/styles"
+import { CharacterActionRollButton } from "./character-action-roll-button"
+import { characterActionLibrary } from "./character-actions"
+
+export function CharacterActionLevelsEditor({
+  readOnly,
+  character,
+  onCharacterChange,
+}: {
+  readOnly: boolean
+  character: Character
+  onCharacterChange: (character: Partial<Character>) => void
+}) {
+  const actionLevels: Record<string, number> = {}
+  if (
+    typeof character.actionLevels === "object" &&
+    character.actionLevels !== null
+  ) {
+    for (const [key, value] of Object.entries(character.actionLevels)) {
+      if (typeof value === "number") {
+        actionLevels[key] = value
+      }
+    }
+  }
+
+  return (
+    <div className="flex flex-wrap gap-4 [&>*]:flex-1 [&>*]:basis-52">
+      {entriesTyped(characterActionLibrary).map(([category, actions]) => (
+        <section
+          key={category}
+          className="flex flex-col rounded-md bg-black/25 p-4"
+        >
+          <h4 className="mb-4 text-center text-xl leading-tight tracking-wide">
+            {category}
+          </h4>
+          <div className="grid gap-4">
+            {actions.map((action) => (
+              <section
+                key={action}
+                className="grid grid-flow-row grid-cols-[1fr,auto] grid-rows-[auto,auto]"
+              >
+                <h5 className={labelTextClass}>{action}</h5>
+
+                <div
+                  className="row-span-2 flex items-end relative gap-2"
+                  style={{ left: "0.25rem" }}
+                >
+                  {!readOnly && (
+                    <>
+                      <CharacterActionRollButton
+                        title={`Roll ${action}`}
+                        intent={`${character.name}: ${action}`}
+                        poolSize={(actionLevels[action] ?? 0) + 1}
+                      >
+                        <Dices />
+                      </CharacterActionRollButton>
+                      <CharacterActionRollButton
+                        title={`Roll ${action} with momentum`}
+                        intent={`${character.name}: ${action} (+1)`}
+                        poolSize={(actionLevels[action] ?? 0) + 2}
+                      >
+                        <ChevronsRight />
+                      </CharacterActionRollButton>
+                    </>
+                  )}
+                </div>
+
+                <DotCounter
+                  value={actionLevels[action] ?? 0}
+                  max={4}
+                  onChange={(level) => {
+                    if (readOnly) return
+                    onCharacterChange({
+                      actionLevels: {
+                        ...actionLevels,
+                        [action]: level,
+                      },
+                    })
+                  }}
+                />
+              </section>
+            ))}
+          </div>
+        </section>
+      ))}
+    </div>
+  )
+}
---
+++ app/routes/worlds.$worldId.characters/character-primary-info-editor.tsx
@@ -0,0 +1,102 @@
+import clsx from "clsx"
+import type { Character } from "../../generated/prisma"
+import { ClockInput } from "../../ui/clock-input"
+import { Counter } from "../../ui/counter"
+import {
+  DebouncedExpandingTextArea,
+  DebouncedInput,
+} from "../../ui/debounced-input"
+import { Field } from "../../ui/field"
+import { inputClass, labelTextClass, textAreaClass } from "../../ui/styles"
+import { CharacterImage } from "./character-image"
+
+export function CharacterPrimaryInfoEditor({
+  character,
+  onCharacterChange,
+  isSpectator,
+  isGameMaster,
+}: {
+  character: Character
+  onCharacterChange: (character: Partial<Character>) => void
+  isSpectator: boolean
+  isGameMaster: boolean
+}) {
+  return (
+    <div className="flex gap-4 flex-wrap [&>*]:basis-48 [&>*]:flex-1">
+      <div className="min-h-[20rem]">
+        <CharacterImage src={character.imageUrl ?? undefined} />
+      </div>
+
+      <div className="grid content-between gap-4">
+        <Field label="Name">
+          <DebouncedInput
+            type="text"
+            placeholder="What should we call you?"
+            value={character.name}
+            onChangeText={(name) => onCharacterChange({ name })}
+            debouncePeriod={500}
+            className={inputClass}
+            readOnly={isSpectator}
+          />
+        </Field>
+        <Field label="Profession(s)">
+          <DebouncedInput
+            type="text"
+            placeholder="Whom do you side with?"
+            value={character.group}
+            onChangeText={(group) => onCharacterChange({ group })}
+            debouncePeriod={500}
+            className={inputClass}
+            readOnly={isSpectator}
+          />
+        </Field>
+        <Field label="Reference image">
+          <DebouncedInput
+            className={inputClass}
+            placeholder="https://the/image.png"
+            value={character.imageUrl ?? ""}
+            onChangeText={(imageUrl) => onCharacterChange({ imageUrl })}
+            debouncePeriod={500}
+            readOnly={isSpectator}
+          />
+        </Field>
+        <section>
+          <h2 className={labelTextClass}>Momentum</h2>
+          <div className={clsx(inputClass, "grid place-items-center")}>
+            {isGameMaster ? (
+              <Counter
+                value={character.momentum}
+                onChange={(momentum) => onCharacterChange({ momentum })}
+              />
+            ) : (
+              <p className="font-medium">{character.momentum}</p>
+            )}
+          </div>
+        </section>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="flex-1 rounded-md bg-black/25 p-4 flex items-center justify-center">
+          <ClockInput
+            name="Stress"
+            progress={character.stress}
+            maxProgress={4}
+            onProgressChange={(stress) => {
+              isGameMaster && onCharacterChange({ stress })
+            }}
+          />
+        </div>
+        <Field label="Condition">
+          <DebouncedExpandingTextArea
+            placeholder="How're you doing?"
+            value={character.condition}
+            onChangeText={(condition) => onCharacterChange({ condition })}
+            debouncePeriod={500}
+            className={textAreaClass}
+            readOnly={isSpectator}
+          />
+        </Field>
+      </div>
+    </div>
+  )
+}
