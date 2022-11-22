b0ef2884f5021cb9694c52624ea4ec6fdb7a49bc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-11-21 21:02:05 -0600

    new form actions class

--- app/routes/worlds.$worldId.settings.tsx
+++ app/routes/worlds.$worldId.settings.tsx
@@ -2,7 +2,6 @@ import type { ActionArgs, LoaderArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
 import type { Params } from "@remix-run/react"
 import {
-  Form,
   useActionData,
   useLoaderData,
   useSubmit,
@@ -12,12 +11,13 @@ import { Minus, Plus, Save, X } from "lucide-react"
 import { useEffect, useId, useRef } from "react"
 import { route } from "routes-gen"
 import { z } from "zod"
+import { zfd } from "zod-form-data"
 import { requireWorldOwner } from "../auth/membership"
 import { requireSessionUser } from "../auth/session.server"
 import { db } from "../core/db.server"
 import { assert } from "../helpers/assert"
 import { discordIdSchema } from "../helpers/discord-id"
-import { FormAction, FormActionGroup } from "../helpers/form"
+import { FormActions } from "../helpers/form-actions"
 import { useDebouncedCallback } from "../helpers/use-debounced-callback"
 import { Field } from "../ui/field"
 import {
@@ -33,114 +33,81 @@ import { getWorld } from "../world/world-db.server"
 const thisRoute = (params: Params) =>
   route("/worlds/:worldId/settings", params as any)
 
-const updateWorld = new FormAction({
-  fields: {
-    name: z.string().max(256),
-  },
-  async action(values, { params }) {
-    await db.world.update({
-      where: { id: params.worldId! },
-      data: {
-        name: values.name,
-      },
-    })
-  },
-})
+type ActionContext = {
+  worldId: string
+}
 
-const addPlayer = new FormAction({
-  fields: {
-    discordUserId: discordIdSchema,
-  },
-  async action(values, { params }) {
-    const data = {
-      role: "PLAYER",
-      world: { connect: { id: params.worldId! } },
-      user: {
-        connectOrCreate: {
-          where: { discordId: values.discordUserId },
-          create: { discordId: values.discordUserId, name: "Unknown" },
+const SettingsActions = FormActions.create<ActionContext>()
+  .withAction("updateWorld", {
+    input: zfd.formData({ name: zfd.text() }),
+    callback: async ({ name }, { worldId }) => {
+      await db.world.update({
+        where: { id: worldId },
+        data: {
+          name,
         },
-      },
-    } as const
-
-    await db.membership.upsert({
-      where: {
-        worldId_userDiscordId: {
-          worldId: params.worldId!,
-          userDiscordId: values.discordUserId,
+      })
+    },
+  })
+  .withAction("addPlayer", {
+    input: zfd.formData({ discordId: zfd.text(discordIdSchema) }),
+    callback: async ({ discordId }, { worldId }) => {
+      const data = {
+        role: "PLAYER",
+        world: { connect: { id: worldId } },
+        user: {
+          connectOrCreate: {
+            where: { discordId },
+            create: { discordId, name: "Unknown" },
+          },
         },
-      },
-      update: data,
-      create: data,
-    })
-  },
-})
+      } as const
 
-const removePlayer = new FormAction({
-  fields: {
-    discordUserId: discordIdSchema,
-  },
-  async action(values, { params }) {
-    await db.membership.delete({
-      where: {
-        worldId_userDiscordId: {
-          worldId: params.worldId!,
-          userDiscordId: values.discordUserId,
+      await db.membership.upsert({
+        where: {
+          worldId_userDiscordId: { worldId, userDiscordId: discordId },
         },
-      },
-    })
-  },
-})
-
-const addCharacterField = new FormAction({
-  fields: {},
-  async action(values, { params }) {
-    await db.characterField.create({
-      data: {
-        name: "New Field",
-        isLong: false,
-        worldId: params.worldId!,
-      },
-    })
-  },
-})
-
-const updateCharacterField = new FormAction({
-  fields: {
-    id: z.string(),
-    name: z.string().max(256),
-    isLong: z
-      .enum(["on"])
-      .optional()
-      .transform((v) => v === "on"),
-  },
-  async action({ id, ...data }, { params }) {
-    await db.characterField.update({
-      where: { id },
-      data,
-    })
-  },
-})
-
-const removeCharacterField = new FormAction({
-  fields: {
-    id: z.string(),
-  },
-  async action({ id }, { params }) {
-    await db.characterField.delete({ where: { id } })
-  },
-})
-
-const actionGroup = new FormActionGroup({
-  updateWorld,
-
-  addPlayer,
-  removePlayer,
-
-  addCharacterField,
-  updateCharacterField,
-  removeCharacterField,
-})
+        update: data,
+        create: data,
+      })
+    },
+  })
+  .withAction("removePlayer", {
+    input: zfd.formData({ discordId: zfd.text(discordIdSchema) }),
+    callback: async ({ discordId }, { worldId }) => {
+      await db.membership.delete({
+        where: {
+          worldId_userDiscordId: { worldId, userDiscordId: discordId },
+        },
+      })
+    },
+  })
+  .withAction("addCharacterField", {
+    callback: async (data, { worldId }) => {
+      await db.characterField.create({
+        data: { name: "New Field", isLong: false, worldId },
+      })
+    },
+  })
+  .withAction("updateCharacterField", {
+    input: zfd.formData({
+      id: zfd.text(),
+      name: zfd.text(z.string().max(256)),
+      isLong: zfd.checkbox(),
+    }),
+    callback: async ({ id, ...data }, { worldId }) => {
+      await db.characterField.update({
+        where: { id },
+        data,
+      })
+    },
+  })
+  .withAction("removeCharacterField", {
+    input: zfd.formData({ id: zfd.text() }),
+    callback: async ({ id }, { worldId }) => {
+      await db.characterField.delete({ where: { id } })
+    },
+  })
 
 export async function loader({ request, params }: LoaderArgs) {
   assert(params.worldId, "worldId is required")
@@ -188,25 +155,27 @@ export async function action(args: ActionArgs) {
   ])
   await requireWorldOwner(user, world)
 
-  return actionGroup.handleSubmit(args, thisRoute(args.params))
+  return SettingsActions.handleSubmit(
+    args,
+    { worldId: args.params.worldId },
+    thisRoute(args.params),
+  )
 }
 
 export default function SettingsPage() {
   const data = useLoaderData<typeof loader>()
   const actionData = useActionData<typeof action>()
 
+  const updateWorld = SettingsActions.useUi("updateWorld", actionData)
+
   return (
     <div className={maxWidthContainerClass}>
       <div className="grid gap-4 py-4">
         <PageSection title="Overview">
-          <Form method="post" className="grid gap-4">
-            <input {...actionGroup.types.updateWorld} />
-            <Field
-              label="World name"
-              errors={actionData?.updateWorld?.fieldErrors?.name}
-            >
-              <input
-                {...updateWorld.fields.name.input}
+          <updateWorld.Form className="grid gap-4">
+            <Field label="World name" errors={updateWorld.fieldErrors?.name}>
+              <updateWorld.Input
+                name="name"
                 defaultValue={data.world.name}
                 className={inputClass}
                 placeholder="My Awesome World"
@@ -217,12 +186,12 @@ export default function SettingsPage() {
                 <Save /> Save
               </button>
             </div>
-            {actionData?.updateWorld?.globalError && (
-              <p className={errorTextClass}>
-                {actionData?.updateWorld?.globalError}
+            {updateWorld?.errors?.map((error, index) => (
+              <p key={index} className={errorTextClass}>
+                {error}
               </p>
-            )}
-          </Form>
+            ))}
+          </updateWorld.Form>
         </PageSection>
 
         <PageSection title="Players">
@@ -234,40 +203,7 @@ export default function SettingsPage() {
           </div>
         </PageSection>
 
-        <PageSection title="Character Fields">
-          <div className="grid gap-2">
-            {[
-              actionData?.addCharacterField?.globalError,
-              actionData?.updateCharacterField?.globalError,
-              actionData?.removeCharacterField?.globalError,
-            ]
-              .filter(Boolean)
-              .map((error, index) => (
-                <p key={index} className={errorTextClass}>
-                  {error}
-                </p>
-              ))}
-          </div>
-
-          <div className="grid gap-2">
-            {actionData?.updateCharacterField?.fieldErrors?.name?.map(
-              (error, index) => (
-                <p key={index} className={errorTextClass}>
-                  {error}
-                </p>
-              ),
-            )}
-
-            <CharacterFieldsList />
-
-            <Form method="post">
-              <input {...actionGroup.types.addCharacterField} />
-              <button className={solidButtonClass} type="submit">
-                <Plus /> Add Field
-              </button>
-            </Form>
-          </div>
-        </PageSection>
+        <CharacterFieldsSection />
       </div>
     </div>
   )
@@ -294,24 +230,20 @@ function AddPlayerForm() {
   const transition = useTransition()
   const formRef = useRef<HTMLFormElement>(null)
 
+  const addPlayer = SettingsActions.useUi("addPlayer", actionData)
+
   useEffect(() => {
-    if (transition.state === "idle" && !actionData?.addPlayer?.hasErrors) {
+    if (transition.state === "idle" && !addPlayer.errors) {
       formRef.current!.reset()
     }
-  }, [actionData?.addPlayer?.hasErrors, transition.state])
+  }, [addPlayer.errors, transition.state])
 
   return (
-    <Form method="post" ref={formRef}>
-      <input {...actionGroup.types.addPlayer} />
-      <Field
-        label="Discord user ID"
-        errors={actionData?.addPlayer?.fieldErrors?.discordUserId?.concat(
-          actionData?.addPlayer?.globalError || [],
-        )}
-      >
+    <addPlayer.Form ref={formRef}>
+      <Field label="Discord user ID" errors={addPlayer.fieldErrors?.discordId}>
         <div className="flex gap-2">
-          <input
-            {...addPlayer.fields.discordUserId.input}
+          <addPlayer.Input
+            name="discordId"
             id={fieldId}
             className={inputClass}
             placeholder="123456789012345678"
@@ -323,7 +255,7 @@ function AddPlayerForm() {
           </button>
         </div>
       </Field>
-    </Form>
+    </addPlayer.Form>
   )
 }
 
@@ -333,12 +265,10 @@ function RemovePlayerForm({
   player: { userDiscordId: string; user: { name: string } }
 }) {
   const actionData = useActionData<typeof action>()
+  const removePlayer = SettingsActions.useUi("removePlayer", actionData)
   return (
-    <Form method="post">
-      <input {...actionGroup.types.removePlayer} />
-      <input
-        {...removePlayer.fields.discordUserId.hidden(player.userDiscordId)}
-      />
+    <removePlayer.Form>
+      <removePlayer.HiddenInput name="discordId" value={player.userDiscordId} />
       <div className="flex gap-2">
         <div className="flex-1">
           <p className={inputClass}>
@@ -356,18 +286,62 @@ function RemovePlayerForm({
           <Minus />
         </button>
       </div>
-      {actionData?.removePlayer?.globalError && (
-        <p className={errorTextClass}>
-          {actionData?.removePlayer?.globalError}
+      {removePlayer.errors?.map((error, index) => (
+        <p key={index} className={errorTextClass}>
+          {error}
         </p>
-      )}
-    </Form>
+      ))}
+    </removePlayer.Form>
+  )
+}
+
+function CharacterFieldsSection() {
+  const actionData = useActionData<typeof action>()
+
+  const addCharacterField = SettingsActions.useUi(
+    "addCharacterField",
+    actionData,
+  )
+  const updateCharacterField = SettingsActions.useUi(
+    "updateCharacterField",
+    actionData,
+  )
+  const removeCharacterField = SettingsActions.useUi(
+    "removeCharacterField",
+    actionData,
+  )
+
+  return (
+    <PageSection title="Character Fields">
+      <div className="grid gap-2">
+        {[
+          addCharacterField.errors,
+          updateCharacterField.errors,
+          removeCharacterField.errors,
+        ]
+          .filter(Boolean)
+          .flat()
+          .map((error, index) => (
+            <p key={index} className={errorTextClass}>
+              {error}
+            </p>
+          ))}
+      </div>
+
+      <div className="grid gap-2">
+        <CharacterFieldsList />
+        <updateCharacterField.Form>
+          <button className={solidButtonClass} type="submit">
+            <Plus /> Add Field
+          </button>
+        </updateCharacterField.Form>
+      </div>
+    </PageSection>
   )
 }
 
 function CharacterFieldsList() {
   const data = useLoaderData<typeof loader>()
-  const actionData = useActionData<typeof action>()
 
   return (
     <div
@@ -400,21 +374,30 @@ function CharacterFieldForm({
 }: {
   field: { id: string; name: string; isLong: boolean }
 }) {
+  const actionData = useActionData<typeof action>()
   const formRef = useRef<HTMLFormElement>(null)
-  const submit = useSubmit()
 
+  const submit = useSubmit()
   const submitDebounced = useDebouncedCallback(
     () => submit(formRef.current!),
     500,
   )
 
+  const updateCharacterField = SettingsActions.useUi(
+    "updateCharacterField",
+    actionData,
+  )
+  const removeCharacterField = SettingsActions.useUi(
+    "removeCharacterField",
+    actionData,
+  )
+
   return (
-    <Form method="post" className="contents" ref={formRef}>
-      <input {...actionGroup.types.updateCharacterField} />
-      <input {...updateCharacterField.fields.id.hidden(field.id)} />
+    <updateCharacterField.Form className="contents" ref={formRef}>
+      <updateCharacterField.HiddenInput name="id" value={field.id} />
       <div role="cell">
-        <input
-          {...updateCharacterField.fields.name.input}
+        <updateCharacterField.Input
+          name="name"
           defaultValue={field.name}
           className={inputClass}
           placeholder="Field name"
@@ -422,8 +405,8 @@ function CharacterFieldForm({
         />
       </div>
       <div role="cell" className="place-self-center">
-        <input
-          {...updateCharacterField.fields.isLong.input}
+        <updateCharacterField.Input
+          name="isLong"
           required={false}
           type="checkbox"
           defaultChecked={field.isLong}
@@ -431,23 +414,22 @@ function CharacterFieldForm({
         />
       </div>
 
-      <Form
+      <removeCharacterField.Form
         role="cell"
         method="post"
         className="place-self-center leading-none"
       >
-        <input {...actionGroup.types.removeCharacterField} />
-        <input {...removeCharacterField.fields.id.hidden(field.id)} />
+        <removeCharacterField.HiddenInput name="id" value={field.id} />
         <button className={clearButtonClass} title="Remove">
           <X />
         </button>
-      </Form>
+      </removeCharacterField.Form>
 
       <div role="cell" className="place-self-center leading-none">
         <button className={clearButtonClass} type="submit" title="Save">
           <Save />
         </button>
       </div>
-    </Form>
+    </updateCharacterField.Form>
   )
 }
