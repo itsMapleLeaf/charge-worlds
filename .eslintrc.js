81d4c002f1ee5ceb7406a59b9a28a5ab92c3440b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-22 23:04:20 -0600

    auto fix to inline type imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-};
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      { fixStyle: "inline-type-imports" },
+    ],
+  },
+}
