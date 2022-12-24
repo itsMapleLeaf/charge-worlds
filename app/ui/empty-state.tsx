a88bee44f58f67093d1704f9d46f3e709ff4673c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-24 02:59:51 -0600

    EmptyState: make properties optional

--- app/ui/empty-state.tsx
+++ app/ui/empty-state.tsx
@@ -1,10 +1,14 @@
 import { type ReactNode } from "react"
 
-export function EmptyState(props: { title: ReactNode; children: ReactNode }) {
+export function EmptyState(props: { title?: ReactNode; children?: ReactNode }) {
   return (
     <section className="mx-auto w-full max-w-screen-md py-16 px-4 text-center">
-      <h2 className="mb-2 text-4xl font-light opacity-50">{props.title}</h2>
-      <div className="text-lg font-light">{props.children}</div>
+      {!!props.title && (
+        <h2 className="mb-2 text-4xl font-light opacity-50">{props.title}</h2>
+      )}
+      {!!props.children && (
+        <div className="text-lg font-light">{props.children}</div>
+      )}
     </section>
   )
 }
