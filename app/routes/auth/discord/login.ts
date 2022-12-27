f2736729da02465c63cefe6c9cd2467bf48c6388
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-27 13:15:22 -0600

    actually redirect though

--- app/routes/auth/discord/login.ts
+++ app/routes/auth/discord/login.ts
@@ -2,5 +2,8 @@ import { type ActionArgs } from "@remix-run/node"
 import { authenticateWithDiscord } from "~/auth.server"
 
 export async function loader({ request }: ActionArgs) {
-  return authenticateWithDiscord(request)
+  return authenticateWithDiscord(request, {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
 }
