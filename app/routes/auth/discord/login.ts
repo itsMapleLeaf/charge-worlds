e22d84deff0cc82af0f6a6e14b3ccb36d3543ff4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-27 12:48:20 -0600

    remove login loader?

--- app/routes/auth/discord/login.ts
+++ app/routes/auth/discord/login.ts
@@ -1,9 +1,5 @@
-import { type LoaderArgs, type ActionArgs } from "@remix-run/node"
+import { type ActionArgs } from "@remix-run/node"
 import { authenticateWithDiscord } from "~/auth.server"
-
-export async function loader({ request }: LoaderArgs) {
-  return authenticateWithDiscord(request)
-}
 
 export async function action({ request }: ActionArgs) {
   return authenticateWithDiscord(request)
