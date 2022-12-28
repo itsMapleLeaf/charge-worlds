1d2b37294e9c85520a44f5ad8f18e1e92870de2e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-12-28 14:12:49 -0600

    actually use prettier to format lol
    it handles non-js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  plugins: ["tailwindcss", "prettier"],
+  plugins: ["tailwindcss"],
   extends: [
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "plugin:tailwindcss/recommended",
   ],
   rules: {
-    "prettier/prettier": "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       { fixStyle: "inline-type-imports" },
