96eb9fa1d17afc72145ae1efc446f54aae40aff1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-02-10 19:44:14 -0600

    upgrades

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,8 @@
 /* eslint-disable unicorn/prefer-module */
-const { flatRoutes } = require("remix-flat-routes")
-
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   devServerPort: 8002,
-  ignoredRouteFiles: ["**/*"],
-  routes: async (defineRoutes) => flatRoutes("routes", defineRoutes),
+  future: {
+    v2_routeConvention: true,
+  }
 }
