8a9754ceb8edbb45713908e9404b0531ca9c887b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-02-12 17:39:54 -0600

    remove old sse/invalidation stuff

--- app/routes/worlds.$worldId.dashboard.tsx
+++ app/routes/worlds.$worldId.dashboard.tsx
@@ -1,17 +1,10 @@
 import type { LoaderArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
-import { useLoaderData, useParams } from "@remix-run/react"
-import { getSessionUser } from "../auth/session.server"
+import { useLoaderData } from "@remix-run/react"
 import { dashboardModules } from "../dashboard/dashboard-modules"
 import { DashboardMosaic } from "../dashboard/dashboard-ui"
-import { parseKeys } from "../helpers/parse-keys"
-import { pick } from "../helpers/pick"
-import { useInvalidate } from "./invalidate"
-import { useWorldEvents } from "./worlds.$worldId.events"
 
 export async function loader({ request, params, context }: LoaderArgs) {
-  const user = await getSessionUser(request)
-
   const moduleData: Record<string, any> = {}
   await Promise.all(
     Object.entries(dashboardModules).map(async ([moduleName, module]) => {
@@ -23,22 +16,10 @@ export async function loader({ request, params, context }: LoaderArgs) {
     }),
   )
 
-  return json({
-    user: user && pick(user, ["id"]),
-    moduleData,
-  })
+  return json({ moduleData })
 }
 
 export default function DashboardPage() {
   const data = useLoaderData<typeof loader>()
-  const { worldId } = parseKeys(useParams(), ["worldId"])
-
-  const invalidate = useInvalidate()
-  useWorldEvents(worldId, (event) => {
-    if (event.sourceUserId !== data.user?.id) {
-      invalidate()
-    }
-  })
-
   return <DashboardMosaic moduleData={data.moduleData} />
 }
