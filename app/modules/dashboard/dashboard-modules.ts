e48b050b68a8c3542a30ab9498689b133bef620d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-03-04 18:13:41 -0600

    remove old dashboard code

--- app/modules/dashboard/dashboard-modules.ts
+++
@@ -1,9 +0,0 @@
-import { charactersModule } from "../characters/characters-module"
-import { clocksModule } from "../clocks/clocks-module"
-import { diceModule } from "../dice/dice-module"
-
-export const dashboardModules = {
-  characters: charactersModule,
-  clocks: clocksModule,
-  dice: diceModule,
-}
