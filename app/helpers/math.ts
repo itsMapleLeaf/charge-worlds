23b7e701ceb51b091f6558d1975720efb8760f9f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-22 13:23:23 -0500

    clamp t when lerping

--- app/helpers/math.ts
+++ app/helpers/math.ts
@@ -1,4 +1,5 @@
-export const lerp = (a: number, b: number, t: number) => a + (b - a) * t
+export const lerp = (a: number, b: number, t: number) =>
+  a + (b - a) * clamp(t, 0, 1)
 
 export const clamp = (value: number, min: number, max: number) =>
   value < min ? min : value > max ? max : value
