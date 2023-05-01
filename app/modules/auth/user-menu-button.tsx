56079eae866c4170430b965cabefcc9e118009aa
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-30 20:12:03 -0500

    fix user menu with this dumb mount transition hook

--- app/modules/auth/user-menu-button.tsx
+++ app/modules/auth/user-menu-button.tsx
@@ -2,6 +2,7 @@ import { Form } from "@remix-run/react"
 import { LogOut, User } from "lucide-react"
 import { Button, Dialog, DialogTrigger, Popover } from "react-aria-components"
 import { route } from "routes-gen"
+import { useMountTransition } from "~/helpers/use-mount-transition"
 import type { AuthContextUser } from "../app/auth"
 
 export type UserMenuButtonProps = {
@@ -9,9 +10,12 @@ export type UserMenuButtonProps = {
 }
 
 export function UserMenuButton({ user }: UserMenuButtonProps) {
+  const { isMounted, handleToggle, isTransitionVisible, elementRef } =
+    useMountTransition()
+
   return (
-    <DialogTrigger>
-      <Button className="block focus:ring-0 data-[focus-visible]:ring-2">
+    <DialogTrigger isOpen={isMounted} onOpenChange={handleToggle}>
+      <Button className="block rounded-full focus-visible:ring-0 !data-[focus-visible]:ring-2">
         {user.avatarUrl ? (
           <img
             src={user.avatarUrl}
@@ -24,19 +28,21 @@ export function UserMenuButton({ user }: UserMenuButtonProps) {
         <span className="sr-only">User menu</span>
       </Button>
       <Popover
-        className="animate-from-opacity-0 animate-from-scale-95 data-[entering]:animate-in data-[exiting]:animate-out"
+        data-visible={isTransitionVisible || undefined}
+        className="origin-top-right scale-95 opacity-0 transition data-[visible]:scale-100 data-[visible]:opacity-100"
         placement="bottom end"
+        ref={elementRef}
       >
-        <Dialog className="panel origin-top-right focus:ring-0 data-[focus-visible]:ring-2">
+        <Dialog className="origin-top-right panel focus:ring-0 !data-[focus-visible]:ring-2">
           <Form
             method="POST"
             action={route("/auth/logout")}
             reloadDocument
             className="contents"
           >
-            <button className="flex items-center gap-2 p-3 leading-none transition data-[highlighted]:bg-black/25">
+            <Button className="flex items-center gap-2 p-3 leading-none transition data-[hovered]:text-accent-300 data-[pressed]:text-accent-100 data-[pressed]:duration-0">
               <LogOut /> Sign out
-            </button>
+            </Button>
           </Form>
         </Dialog>
       </Popover>
