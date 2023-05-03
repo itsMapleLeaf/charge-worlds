c9b85ddedfa65706e29833bff81000b008857085
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-05-03 09:10:33 -0500

    disable dev server?

--- remix.config.cjs
+++ remix.config.cjs
@@ -9,7 +9,7 @@ const config = {
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
-    unstable_dev: true,
+    // unstable_dev: true,
   },
   watchPaths: ["./uno.config.ts"],
 }
