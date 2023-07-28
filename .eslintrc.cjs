04f311839c3d92f60fffa0d59b3ec46e7b62f67b
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-07-28 12:17:44 -0500

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,17 @@ module.exports = {
     browser: true,
     node: true,
   },
-  ignorePatterns: ["node_modules/**", "build/**", "public/build/**"],
+  ignorePatterns: ["node_modules/**", "build/**", "public/build/**", "convex"],
   parserOptions: {
     project: require.resolve("./tsconfig.json"),
   },
+  overrides: [
+    {
+      files: ["convex/**"],
+      excludedFiles: ["convex/_generated/**"],
+      parserOptions: {
+        project: require.resolve("./convex/tsconfig.json"),
+      },
+    },
+  ],
 }
