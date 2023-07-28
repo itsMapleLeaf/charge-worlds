2865cc1956e035e5bdac87bc0511a56083ed9dda
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-07-28 14:06:55 -0500

    organize panda config

--- panda.config.ts
+++ panda.config.ts
@@ -10,34 +10,10 @@ const fadeRiseKeyframes = {
 }
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
-
-  // The extension for the emitted JavaScript files
   outExtension: "js",
-  // Where to look for your css declarations
   include: ["./app/**/*.{ts,tsx,js,jsx}"],
-
-  // Files to exclude
   exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {
-      tokens: {
-        colors: {
-          base: preset.theme.tokens.colors.neutral,
-          accent: preset.theme.tokens.colors.indigo,
-        },
-        fonts: {
-          sans: { value: `"Spline Sans Variable", sans-serif` },
-        },
-      },
-      keyframes: fadeRiseKeyframes,
-    },
-  },
-
-  // The output directory for your css system
   outdir: "styled-system",
 
   globalCss: defineGlobalStyles({
@@ -53,4 +29,19 @@ export default defineConfig({
       color: "inherit",
     },
   }),
+
+  theme: {
+    extend: {
+      tokens: {
+        colors: {
+          base: preset.theme.tokens.colors.neutral,
+          accent: preset.theme.tokens.colors.indigo,
+        },
+        fonts: {
+          sans: { value: `"Spline Sans Variable", sans-serif` },
+        },
+      },
+      keyframes: fadeRiseKeyframes,
+    },
+  },
 })
