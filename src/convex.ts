5d20d2da4d061ddd88dad9298fa42b51db6d93f2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-07-31 23:07:59 -0500

    don't need a cache apparently

--- src/convex.ts
+++ src/convex.ts
@@ -1,43 +1,17 @@
 import { raise } from "convex/helpers"
 import { ConvexReactClient, Watch } from "convex/react"
-import {
-	FunctionReference,
-	FunctionReturnType,
-	OptionalRestArgs,
-} from "convex/server"
+import { FunctionReference, OptionalRestArgs } from "convex/server"
 import { useEffect, useState } from "react"
 
 export const convexClient = new ConvexReactClient(
 	import.meta.env.VITE_CONVEX_URL,
 )
 
-const cache = new Map<FunctionReference<"query">, Map<string, Watch<unknown>>>()
-
-function getWatch<FuncRef extends FunctionReference<"query">>(
-	reference: FuncRef,
-	...args: OptionalRestArgs<FuncRef>
-) {
-	let referenceCache = cache.get(reference)
-	if (!referenceCache) {
-		referenceCache = new Map()
-		cache.set(reference, referenceCache)
-	}
-
-	const key = JSON.stringify(args)
-	let watch = referenceCache.get(key)
-	if (!watch) {
-		watch = convexClient.watchQuery(reference, ...args)
-		referenceCache.set(key, watch)
-	}
-
-	return watch as Watch<FunctionReturnType<FuncRef>>
-}
-
 export function useQuerySuspense<FuncRef extends FunctionReference<"query">>(
 	reference: FuncRef,
 	...args: OptionalRestArgs<FuncRef>
 ) {
-	const watch = getWatch(reference, ...args)
+	const watch = convexClient.watchQuery(reference, ...args)
 
 	const initialValue = watch.localQueryResult()
 	if (!initialValue) {
